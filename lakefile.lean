d94775de0b87907607bb43131c9273a6febbca46
Thomas Marchand <thomas@lfglabs.dev>
2026-05-15 11:06:18 +0200

    Update UnlinkPool ergonomics Verity pin

--- lakefile.lean
+++ lakefile.lean
@@ -5,7 +5,7 @@ package «verity-benchmark» where
   version := v!"0.1.0"
 
 require verity from git
-  "https://github.com/lfglabs-dev/verity.git"@"02622d1f0d647f23c653caf28c4747080a129eb7"
+  "https://github.com/lfglabs-dev/verity.git"@"551ab45c7c2fefcdac385946d22758ff3854c00c"
 
 @[default_target]
 lean_lib «Benchmark» where
